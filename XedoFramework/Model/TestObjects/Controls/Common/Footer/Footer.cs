0cad771988c0ff4e19543da6e7da87f723ea28c9
James Fraser <james.fraser@xedosoftware.com>
2016-06-28 14:24:37 +0100

    Merging

--- XedoFramework/Model/TestObjects/Controls/Common/Footer/Footer.cs
+++
@@ -1,9 +0,0 @@
-﻿using XedoFramework.Model.TestObjects.Bases;
-
-namespace XedoFramework.Model.TestObjects.Controls.Common.Footer
-{
-    public class Footer : ControlBase
-    {
-        public Footer(TestSettings testSettings) : base(testSettings) { }
-    }
-}
